874e5e9f2f9de0d9a32d69d4a8b87af3d537677c
christopherfb <christopherfb@gmail.com>
2025-06-07 23:11:59 -0700

    completed video 30 : Vertex Buffer Layouts
    added an abstraction layer for the indices and vertices that currently are being set up in Application.cpp.

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -9,6 +9,7 @@
 #include "Hazel/ImGui/ImGuiLayer.h"
 
 #include "Hazel/Renderer/Shader.h"
+#include "Hazel/Renderer/Buffer.h"
 
 namespace Hazel {
 
@@ -38,11 +39,12 @@ namespace Hazel {
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray;
-		unsigned int m_VertexBuffer;
-		unsigned int m_IndexBuffer;
+		//unsigned int m_VertexBuffer;
+		//unsigned int m_IndexBuffer;
 
 		std::unique_ptr<Shader> m_Shader;
-
+		std::unique_ptr<VertexBuffer> m_VertexBuffer;
+		std::unique_ptr<IndexBuffer> m_IndexBuffer;
 		
 	private:
 		static Application* s_Instance;
---
+++ Hazel/src/Hazel/Renderer/Buffer.cpp
@@ -0,0 +1,41 @@
+#include "hzpch.h"
+#include "Buffer.h"
+#include "Renderer.h"
+#include "Platform/OpenGL/OpenGLBuffer.h"
+
+namespace Hazel {
+
+	VertexBuffer* Hazel::VertexBuffer::Create(float* vertices, uint32_t size)
+	{
+		switch (Renderer::GetAPI()) {
+		case RendererAPI::None: 
+			HZ_CORE_ASSERT(false, "RenderAPI::None is not supported.");
+			return nullptr;
+			break;
+
+		case RendererAPI::OpenGL:
+			return new OpenGLVertexBuffer(vertices, size);
+			break;
+
+		}
+		HZ_CORE_ASSERT(false, "Unknown RenderAPI!");
+		return nullptr;
+	}
+
+	IndexBuffer* Hazel::IndexBuffer::Create(uint32_t* indices, uint32_t size)
+	{
+		switch (Renderer::GetAPI()) {
+		case RendererAPI::None:
+			HZ_CORE_ASSERT(false, "RenderAPI::None is not supported.");
+			return nullptr;
+			break;
+
+		case RendererAPI::OpenGL:
+			return new OpenGLIndexBuffer(indices, size);
+			break;
+
+		}
+		HZ_CORE_ASSERT(false, "Unknown RenderAPI!");
+		return nullptr;
+	}
+}
