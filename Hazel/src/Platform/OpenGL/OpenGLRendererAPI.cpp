3d77cf1ae93b3e9afd6782e950e47a591ee18e8c
DESKTOP-5H8KVD0\Chris <christopherfb@gmail.com>
2025-06-16 15:19:01 -0700

    completed video 41: Refs, Scopes, and Smart Pointers
    changed std::shared_ptr in Hazel to it's own class called Ref

--- Hazel/src/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -12,7 +12,7 @@ void Hazel::OpenGLRendererAPI::Clear()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
-void Hazel::OpenGLRendererAPI::DrawIndexed(const std::shared_ptr<VertexArray>& vertexArray)
+void Hazel::OpenGLRendererAPI::DrawIndexed(const Ref<VertexArray>& vertexArray)
 {
 	glDrawElements(GL_TRIANGLES, vertexArray->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
 }
